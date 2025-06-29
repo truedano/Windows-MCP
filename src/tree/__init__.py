84b1738c9e175cb777f45018971224a334001ecb
Jeomon <jeogeoalukka@gmail.com>
2025-06-29 17:29:02 +0530

    more check to handle the corner cases of UI elements

--- src/tree/__init__.py
+++ src/tree/__init__.py
@@ -1,5 +1,5 @@
 from src.tree.views import TreeElementNode, TextElementNode, ScrollElementNode, Center, TreeState, BoundingBox
-from src.tree.config import INTERACTIVE_CONTROL_TYPE_NAMES,INFORMATIVE_CONTROL_TYPE_NAMES
+from src.tree.config import INTERACTIVE_CONTROL_TYPE_NAMES,INFORMATIVE_CONTROL_TYPE_NAMES, DEFAULT_ACTIONS
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from uiautomation import GetRootControl,Control,ImageControl
 from src.desktop.config import AVOIDED_APPS
@@ -49,13 +49,24 @@ class Tree:
         interactive_nodes, informative_nodes, scrollable_nodes = [], [], []
         app_name=node.Name.strip()
         app_name='Desktop' if app_name=='Program Manager' else app_name
+        
         def is_element_interactive(node:Control):
             try:
                 if node.ControlTypeName in INTERACTIVE_CONTROL_TYPE_NAMES:
                     if is_element_visible(node) and is_element_enabled(node) and not is_element_image(node):
                         return True
+                elif node.ControlTypeName=='GroupControl':
+                    if is_element_visible(node) and is_element_enabled(node) and is_default_action(node):
+                        return True
             except Exception:
                 return False
+            return False
+        
+        def is_default_action(node:Control):
+            legacy_pattern=node.GetLegacyIAccessiblePattern()
+            default_action=legacy_pattern.DefaultAction
+            if default_action in DEFAULT_ACTIONS:
+                return True
             return False
         
         def is_element_visible(node:Control,threshold:int=0):
